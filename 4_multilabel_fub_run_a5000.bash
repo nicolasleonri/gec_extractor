e885c13ac6e8159d136d1ce2029e681b6c6e3da9
Leon Rios <nicolasal97@login.curta.zedat.fu-berlin.de>
2025-11-02 23:23:05 +0100

    test

---
+++ 4_multilabel_fub_run_a5000.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=multilabel_fub_run_a5000
+#SBATCH --output=./logs/slurm/output_fub_multilabel_a5000_%j.out
+
+#SBATCH --partition=scavenger
+#SBATCH --account=agfritz
+#SBATCH --qos=standard
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:a5000:1
+#SBATCH --mem=4G
+
+#SBATCH --time=01:00:00
+
+echo "Loading modules..."
+module purge
+module load CUDA/12.1.1
+module load cuDNN/8.9.2.26-CUDA-12.1.1
+module load virtualenv/20.26.2-GCCcore-13.3.0
+
+echo "Setting folders..."
+export HF_HOME=/scratch/nicolasal97/.cache/huggingface
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+echo "Activating virtual environment..."
+source venv/multi_label/bin/activate
+
+echo "Running python script..."
+python3 -u ./src/multi_label/multi_label.py -f ./data/csv/multi_label/etiquetado_perspectivas.csv -ns 600 -mp /scratch/nicolasal97/gec_extractor/results/models/multi_label
+
+deactivate
+module purge
+
+echo "Script finished successfully"
